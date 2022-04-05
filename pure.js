f4f0129adfd0eafe8ebc43b2c4a31ad2a1f9b639
污斑兔 <zweizhao@foxmail.com>
2022-04-05 18:23:55 +0800

    更新命名空间模式

--- pure.js
+++ pure.js
@@ -1,78 +1,106 @@
-// template标签根据ES6模板字符串转HTML标签
-String.prototype.template2string = function (params) {
-  const keys = Object.keys(params)
-  const values = Object.values(params)
-  return new Function(...keys, `return \`${this}\``)(...values)
-}
-
-// 移除常见中文标点
-String.prototype.removePunctuation = function () {
-  return this.replace(/，|。|？|！|；|、！|：|“|”|《|【|】|》/g, '&emsp;')
-}
-
-// 缓存
-window.cache = {}
-
-// 判断是否为华为快应用
-window.is_hwqa = typeof system !== 'undefined'
-
-window.$ = function (selector) {
-  return document.querySelector(selector)
-}
-
-window.$$ = function (selector, is_normal) {
-  const doms = document.querySelectorAll(selector)
-  return is_normal ? doms : Array.from(doms)
-}
-
-window.loading = function () {
-  if (window.is_hwqa) {
-    system.postMessage('loading---请求中...')
-  } else {
-    document.body.style.pointerEvents = 'none'
-    document.body.style.opacity = 0.2
+;(function () {
+  function PureJS() {
+    this.util.$$('[id]').forEach((dom) => {
+      this.dom[dom.id] = dom
+    })
   }
-}
 
-window.unloading = function () {
-  if (window.is_hwqa) {
-    system.postMessage('unloading')
-  } else {
-    document.body.style.pointerEvents = 'initial'
-    document.body.style.opacity = 1
+  // 功能函数
+  PureJS.prototype.util = {
+    $(selector) {
+      return document.querySelector(selector)
+    },
+    $$(selector, is_normal) {
+      const doms = document.querySelectorAll(selector)
+      return is_normal ? doms : Array.from(doms)
+    },
+    // template标签根据ES6模板字符串转HTML标签
+    template2string(string, params) {
+      const keys = Object.keys(params)
+      const values = Object.values(params)
+      return new Function(...keys, `return \`${string}\``)(...values)
+    },
+    // 移除常见中文标点
+    removePunctuation(string) {
+      return string.replace(/，|。|？|！|；|、！|：|“|”|《|【|】|》/g, '&emsp;')
+    },
+    // 创建子类
+    createSubClass(BaseClass = PureJS) {
+      function SubClass(...args) {
+        BaseClass.apply(this, args)
+      }
+      const InheritSuper = function () {}
+      InheritSuper.prototype = BaseClass.prototype
+      SubClass.prototype = new InheritSuper()
+      SubClass.prototype.constructor = BaseClass
+      return SubClass
+    },
   }
-}
-
-let window_alert = window.alert
-window.alert = function (text) {
-  if (window.is_hwqa) {
-    system.postMessage('alert---' + text)
-  } else {
-    return window_alert.call(null, text)
+  // 通用配置
+  PureJS.prototype.config = {
+    // 判断是否为华为快应用
+    is_hwqa: typeof system !== 'undefined',
   }
-}
+  // 统一数据处理
+  PureJS.prototype.data = {}
+  // 统一DOM处理
+  PureJS.prototype.dom = {}
+  // 统一回调
+  PureJS.prototype.callback = {}
+  // 统一缓存处理
+  PureJS.prototype.cache = {}
+  // UI交互处理
+  PureJS.prototype.ui = {
+    loading() {
+      if (this.config.is_hwqa) {
+        system.postMessage('loading---请求中...')
+      } else {
+        document.body.style.pointerEvents = 'none'
+        document.body.style.opacity = 0.2
+      }
+    },
+    unloading() {
+      if (this.config.is_hwqa) {
+        system.postMessage('unloading')
+      } else {
+        document.body.style.pointerEvents = 'initial'
+        document.body.style.opacity = 1
+      }
+    },
+    alert(text) {
+      if (this.config.is_hwqa) {
+        system.postMessage('alert---' + text)
+      } else {
+        return window.alert.call(null, text)
+      }
+    },
+  }
+  // 处理函数
+  PureJS.prototype.update = function (data) {
+    let value, value_origin, callback
+    for (const key in data) {
+      if (data.hasOwnProperty(key)) {
+        value = data[key]
+        value_origin = this.data[key]
+        callback = this.callback[key]
+        if (value !== value_origin) {
+          this.data[key] = value
+          callback && callback(value, data)
+        }
+      }
+    }
+  }
+  PureJS.prototype.regist = function (key, callback) {
+    this.callback[key] = callback.bind(this)
+  }
+  PureJS.prototype.unregist = function (key) {
+    this.callback[key] = null
+  }
+  Pure.prototype.remove = function(key) {
+    this.data[key] = null
+    this.dom[key] = null
+    this.callback[key] = null
+  }
 
-function Page() {
-  this.dom = {}
-
-  $$('[id]').forEach((dom) => {
-    this.dom[dom.id] = dom
-  })
-
-  this.data = null
-  this.data_proxy = null
-  if(this.dataHandler) this.dataHandler()
-  else console.error('请实现dataHandler方法')
-}
-
-function App() {
-  Page.call(this, arguments)
-}
-
-// 避免两次调用继承的父类
-function InheritSuper() {}
-InheritSuper.prototype = Page.prototype
-App.prototype = new InheritSuper()
-App.prototype.constructor = Page
-
-var app = new App()
+  window.pjs = new PureJS()
+})()
